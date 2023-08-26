c3ca9bf3f50dabdeaf306f3474f8dd8eaea58e1b
Sanjaypranav <sanjaypriya195@gmail.com>
2023-08-26 10:05:22 +0530

    updated to dynamic db

--- app.py
+++ app.py
@@ -33,22 +33,27 @@ def get_data():
         cur = conn.cursor()
         cur.execute("SELECT * FROM mytable")
         data = cur.fetchall()
-        df = pd.DataFrame(data, columns=[
-            "fixed acidity",
-            "volatile acidity",
-            "citric acid",
-            "residual sugar",
-            "chlorides",
-            "free sulfur dioxide",
-            "total sulfur dioxide",
-            "density",
-            "pH",
-            "sulphates",
-            "alcohol",
-            "quality"
-        ])
-        df.to_csv('data.csv', index=False)
-        return jsonify(df.to_dict(orient='records'))
+        column_names = [desc[0] for desc in cur.description]  # Get column names
+        
+        data_with_columns = {"columns": column_names, "data": data}
+        print(data_with_columns)
+        return jsonify(data_with_columns)
+        # df = pd.DataFrame(data, columns=[
+        #     "fixed acidity",
+        #     "volatile acidity",
+        #     "citric acid",
+        #     "residual sugar",
+        #     "chlorides",
+        #     "free sulfur dioxide",
+        #     "total sulfur dioxide",
+        #     "density",
+        #     "pH",
+        #     "sulphates",
+        #     "alcohol",
+        #     "quality"
+        # ])
+        # df.to_csv('data.csv', index=False)
+        # return jsonify(df.to_dict(orient='records'))
         # cur.close()
         # conn.close()
         # return jsonify(data)
